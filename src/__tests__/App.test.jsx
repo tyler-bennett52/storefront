0e969f29554d9ad558da14e757a6f30bb57e1878
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-24 23:07:56 -0500

    introduce SimpleCart into integration test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -5,16 +5,22 @@ import store from '../store';
 import { Provider } from 'react-redux';
 import Products from '../Components/Products';
 import Categories from '../Components/Categories';
+import SimpleCart from '../Components/SimpleCart';
 
-describe('Products and Categories components', () => {
+describe('Products, Categories, and SimpleCart components', () => {
+  beforeEach(() => {
     render(
       <Provider store={store}>
-        <Categories />
-        <Products />
+        <div style={{ position: 'relative' }}>
+          <Categories />
+          <Products />
+          <SimpleCart />
+        </div>
       </Provider>
     );
+  });
 
-  it('renders categories buttons and interacts with Products', () => {
+  it('renders categories buttons and interacts with Products and SimpleCart', () => {
     expect(screen.getByText('Electronics')).toBeInTheDocument();
 
     // Test if clicking a category button changes the selected category
@@ -26,8 +32,19 @@ describe('Products and Categories components', () => {
     fireEvent.click(screen.getByText('Clothing'));
     expect(screen.queryByText('TV')).not.toBeInTheDocument();
 
-    // Test if clicking the "Reset" button clears the selected category
+    // Test if clicking the "Add to cart" button adds a product to the cart
+    const addToCartButtons = screen.getAllByText('Add to cart');
+    fireEvent.click(addToCartButtons[0]);
+    expect(store.getState().cart).toEqual({"cart": [{"category": "clothing", "inStock": 25, "name": "Shirt", "price": 9}]});
+
+    // Test if clicking the "Remove from cart" button removes a product from the cart
+    const removeFromCartButton = screen.getByLabelText('remove from cart');
+    fireEvent.click(removeFromCartButton);
+    expect(store.getState().cart).toEqual({"cart": []});
+
+    // Test if clicking the "Reset" button clears the selected category and cart
     fireEvent.click(screen.getByText('Reset'));
     expect(screen.getByTestId('selected-category')).toHaveTextContent('');
+    expect(store.getState().cart).toEqual({"cart": []});
   });
 });
