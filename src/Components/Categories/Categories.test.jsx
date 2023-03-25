643951a06118a49a559cadc958671cf67741b698
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-25 16:52:44 -0500

    Categories unit test

---
+++ src/Components/Categories/Categories.test.jsx
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Categories from './';
+
+const mockStore = configureStore([thunk]);
+const initialState = {
+  categories: {
+    categories: [
+      { name: 'electronics', displayName: 'Electronics' },
+      { name: 'food', displayName: 'Food' },
+      { name: 'clothing', displayName: 'Clothing' },
+    ],
+  },
+};
+
+const store = mockStore(initialState);
+
+describe('Categories component', () => {
+  it('renders category buttons', () => {
+    render(
+      <Provider store={store}>
+        <Categories />
+      </Provider>
+    );
+
+    expect(screen.getByText('electronics')).toBeInTheDocument();
+    expect(screen.getByText('food')).toBeInTheDocument();
+    expect(screen.getByText('clothing')).toBeInTheDocument();
+  });
+
+  it('dispatches changeCategory action when a button is clicked', () => {
+    render(
+      <Provider store={store}>
+        <Categories />
+      </Provider>
+    );
+
+    fireEvent.click(screen.getByText('electronics'));
+
+    const dispatchedActions = store.getActions();
+    expect(dispatchedActions[0]).toEqual({ type: 'change-category', payload: 'electronics' });
+  });
+});
